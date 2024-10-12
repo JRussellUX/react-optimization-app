3e57271078db7089d38879163eb5bed33671d858
Jen Russell <jennifer@jrussellux.com>
2024-10-12 14:00:30 -0700

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Quote from "./Quote";
+
+//function getRandomQuote(quotes) {
+//  return quotes[Math.floor(Math.random() * quotes.length)];
+//}
 
 function App() {
+  const [quote, setQuote] = useState(null);
+
+  function getNewQuote() {
+    fetch("http://thesimpsonsquoteapi.glitch.me/quotes")
+      .then((res) => res.json())
+      .then((json) => {
+        setQuote(json[0]);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Weekly Hands-on Activity: Simpsons Quote Generator</h1>
+      <section>
+        <button onClick={getNewQuote}>New Quote</button>
+        <Quote quote={quote}></Quote>
+      </section>
+    </main>
   );
 }
 
